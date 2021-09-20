813ae3c803bbc396e999cec1590b97005e74e6bd
nemo <603035634@qq.com>
2021-09-20 19:03:05 +0800

    .

--- main.cpp
+++ main.cpp
@@ -7,23 +7,23 @@ volatile int counter(0); // non-atomic counter
 std::mutex mtx;           // locks access to counter
 
 void attempt_10k_increases() {
-    for (int i = 0; i < 10000; ++i) {
-        if (mtx.try_lock()) {   // only increase if currently not locked:
-            ++counter;
-            mtx.unlock();
-        }
-    }
+	for (int i = 0; i < 10000; ++i) {
+		if (mtx.try_lock()) {   // only increase if currently not locked:
+			++counter;
+			mtx.unlock();
+		}
+	}
 }
 
 int main()
 {
-    std::thread threads[4];
-    // spawn 10 threads:
-    for (int i = 0; i < 4; ++i)
-        threads[i] = std::thread(attempt_10k_increases);
+	std::thread threads[4];
+	// spawn 10 threads:
+	for (int i = 0; i < 4; ++i)
+		threads[i] = std::thread(attempt_10k_increases);
 
-    for (auto& th : threads) th.join();
-    std::cout << counter << " successful increases of the counter.\n";
+	for (auto& th : threads) th.join();
+	std::cout << counter << " successful increases of the counter.\n";
 
-    return 0;
+	return 0;
 }
